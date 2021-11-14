90cf554655a53330413c434f635ef048f0b3be91
ShutDownMan <Jedson_Gabriel@hotmail.com>
2021-11-14 09:06:52 -0300

    DOCUMENTED tasks file

--- src/Tasks/index.ts
+++ src/Tasks/index.ts
@@ -37,60 +37,48 @@ class Tasks {
     }
 
     public async classReminder() {
-        console.debug("=========================")
-        console.debug("running classReminder...")
-        // console.debug(persistence)
-        // let now = new Date();
-        // let today = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1));
-        // let todaysClassesData = await persistence.fetchClassDataByDate(today)
-
         let todaysClassesData: UniClass[] = await this.client.persistence.fetchTodaysClassData()
 
-        // console.debug(JSON.stringify(todaysClassesData))
-
+        /// for class in todays classes
         for (let uniClass of todaysClassesData) {
 
+            /// if class data is there
             if (uniClass.classData) {
+                /// if reminder was not yet sent
                 if (!uniClass.classData.reminderSent) {
+                    /// get relevant times to check
                     let currentTime = new Date()
                     let classTime = new Date(uniClass.classData.startTime)
                     let reminderTime = new Date(classTime.getTime() - Configs.ClassReminderTimeInMinutes * 60 * 1000)
                     let finishedClassTime = new Date(classTime.getTime() + toSeconds(parse(uniClass.classData.horario.duracao)) * 1000)
 
+                    let classStatus = uniClass.classData.status
 
-                    if (currentTime >= finishedClassTime) {
-                        //^ check if class is finished
+                    /// if class is finished
+                    if (currentTime >= finishedClassTime && (classStatus === ClassStatus.UNSTARTED || classStatus === ClassStatus.ONGOING)) {
+                        console.debug("finished? " + JSON.stringify(uniClass.classData))
 
-                        if (uniClass.classData.status === ClassStatus.UNSTARTED || uniClass.classData.status === ClassStatus.ONGOING) {
-                            console.debug("finished? " + JSON.stringify(uniClass.classData))
-
-                            uniClass.classData.status = ClassStatus.FINISHED
-                            await this.client.persistence.upsertClassData(uniClass.classID, uniClass.classData)
-                        }
-                    }
-
-                    if (currentTime >= classTime && uniClass.classData.status === ClassStatus.UNSTARTED) {
-                        //^ check if class is ongoing
-
-                        // console.debug("ongoing? " + JSON.stringify(uniClass.classData))
-
-                        uniClass.classData.status = ClassStatus.ONGOING
+                        /// update class status
+                        classStatus = ClassStatus.FINISHED
+                        /// persist changes
                         await this.client.persistence.upsertClassData(uniClass.classID, uniClass.classData)
                     }
 
-                    let materia: MateriaData = Materias[uniClass.classData.materiaID]
-                    console.debug(materia.nomeMateria + " passou? " + (currentTime >= reminderTime))
-                    // console.debug("currentTime:" + JSON.stringify(currentTime))
-                    // console.debug("reminderTime:" + JSON.stringify(reminderTime))
-                    // console.debug("classTime:" + JSON.stringify(classTime))
-                    // console.debug("finishedClassTime:" + JSON.stringify(finishedClassTime))
-                    // console.debug(JSON.stringify(toSeconds(parse(uniClass.classData.horario.duracao))))
-                    // console.debug("-------------------------")
+                    /// check if class is ongoing
+                    if (currentTime >= classTime && classStatus === ClassStatus.UNSTARTED) {
 
-                    if (currentTime >= reminderTime && (uniClass.classData.status === ClassStatus.UNSTARTED)) {
-                        //^ check if time to send reminder
+                        /// update class status
+                        classStatus = ClassStatus.ONGOING
+                        /// persist changes
+                        await this.client.persistence.upsertClassData(uniClass.classID, uniClass.classData)
+                    }
 
+                    /// check if time to send reminder
+                    if (currentTime >= reminderTime && (classStatus === ClassStatus.UNSTARTED)) {
+
+                        /// update class status
                         console.debug("reminderTime: " + reminderTime)
+                        /// persist changes
                         await this.sendClassReminder(uniClass, classTime)
                     }
                 }
@@ -101,16 +89,20 @@ class Tasks {
     private async sendClassReminder(uniClass: UniClass, startTime: Date) {
 
         try {
+            /// get materia info
             let materia: Materia = { materiaID: uniClass.classData.materiaID, materiaData: Materias[uniClass.classData.materiaID] }
 
             console.debug("SENDING REMINDER TO " + materia.materiaData.nomeMateria)
 
+            /// get guild and mention from client
             let guild = await this.client.guilds.fetch(process.env.GUILD_ID)
             let mention = await guild.roles.fetch(materia.materiaID)
 
             let message = null
 
+            /// if class is cancelled
             if (uniClass.classData.status !== ClassStatus.CANCELED) {
+                /// custom message if pratica ou teórica
                 if (uniClass.classData.horario.tipoAula === "Teórica")
                     message = `**Olá ${mention},\nA aula de \`${materia.materiaData.nomeMateria}\` vai começar!\n<t:${Math.trunc(startTime.getTime() / 1000)}:R>**`
 
@@ -122,11 +114,13 @@ class Tasks {
 
             uniClass.classData.reminderSent = true
 
+            /// send message to text channel
             let remindMessage = await sendToTextChannel(this.client, materia.materiaData.canalTextoID, message)
             await remindMessage.react("👍")
             await this.showUpcomingReminders(this.client, materia)
             // await sendToTextChannel(this.client, materia.canalTextoID, "https://tenor.com/bab2Y.gif")
 
+            /// persist changes
             await this.client.persistence.upsertClassData(uniClass.classID, uniClass.classData)
         } catch (error) {
             console.error(error)
@@ -145,20 +139,26 @@ class Tasks {
         let interactionAuthor = await guild.members.fetch(process.env.BOT_ID)
 
         let today = moment().startOf('day')
+        /// get only non expired reminders
         reminders = reminders.filter((reminder) => { return today < moment(reminder.reminderData.dueDate) })
+        /// sort them by date asc
         reminders = reminders.sort((a, b) => { return (moment(a.reminderData.dueDate) < moment(b.reminderData.dueDate) ? -1 : 1) })
 
         let embedsToSend: MessageEmbed[] = []
+        /// if there are reminders
         if (reminders.length > 0) {
+            /// divide reminders in chunks of 25
             for (let ind = 0; ind < reminders.length / 25 && ind < 10; ++ind) {
                 let currentReminders = reminders.splice(ind * 25, 25)
 
+                /// create custom desc given available info
                 let embedDesc = ""
                 if (materia) {
                     embedDesc = `Lista de lembretes de escopo \`${ReminderScope.Public}\` da matéria de \`${materia.materiaData.nomeMateria}\`.`
                 } else {
                     embedDesc = `Lista de lembretes de escopo \`${ReminderScope.Public}\`.`
                 }
+                /// create embed
                 let reminderEmbed = new MessageEmbed()
                     .setColor('#4f258a')
                     .setTitle(((ind > 0) ? 'Continuação dos ' : '') + 'Lembretes:')
@@ -166,7 +166,9 @@ class Tasks {
                     .setTimestamp()
                     .setFooter(interactionAuthor.displayName, interactionAuthor.displayAvatarURL());
 
+                /// create an embed field for each reminder
                 for (let reminder of currentReminders) {
+                    /// populate fields
                     let reminderAuthor = await guild.members.fetch(reminder.reminderData.author)
                     let reminderTitle = `Lembrete de ${capitalize(reminder.reminderData.type)} de ${reminderAuthor.displayName}`
                     reminderTitle += (reminder.reminderData.dueDate) ? ` <t:${moment(reminder.reminderData.dueDate).unix()}:R>` : ``;
@@ -176,13 +178,18 @@ class Tasks {
                         [Mensagem](${reminder.reminderData.descriptionURL} 'Link para a mensagem da anotação')\n\u200b\
                     `
 
+                    /// add to embed
                     reminderEmbed.addField(reminderTitle, reminderDesc, false)
                 }
 
+                /// add embed to list of embeds
                 embedsToSend.push(reminderEmbed)
             }
 
         } else {
+            //^ no reminders
+
+            /// create a helpful embed
             let noRemindersEmbed = new MessageEmbed()
                 .setColor('#4f258a')
                 .setTitle('Lembretes:')
@@ -190,22 +197,28 @@ class Tasks {
                 .setTimestamp()
                 .setFooter(interactionAuthor.displayName, interactionAuthor.displayAvatarURL());
 
+            /// add to embed list
             embedsToSend.push(noRemindersEmbed)
         }
 
+        /// populate message fields and send
         let messageContent: any = { content: `**\u200b**`, embeds: embedsToSend, components: [], ephemeral: true, fetchReply: true };
-        let showRemindersMessage = await sendToTextChannel(this.client, materia.materiaData.canalTextoID, messageContent)
+        return await sendToTextChannel(this.client, materia.materiaData.canalTextoID, messageContent)
     }
 
     public async classDiary() {
         let todaysDiary = await this.client.persistence.fetchTodaysDiary()
-        if(!todaysDiary || todaysDiary.diaryData.dailyReminderSent) return;
+        /// if there's no diary for today, exit
+        if (!todaysDiary || todaysDiary.diaryData.dailyReminderSent) return;
 
         let guild = await this.client.guilds.fetch(process.env.GUILD_ID)
         let interactionAuthor = await guild.members.fetch(process.env.BOT_ID)
         let todaysClassesData: UniClass[] = await this.client.persistence.fetchTodaysClassData()
-        todaysClassesData.sort((a, b) => { return (moment(a.classData.startTime) < moment(b.classData.startTime)) ? -1 : 1})
 
+        /// sor classes by time
+        todaysClassesData.sort((a, b) => { return (moment(a.classData.startTime) < moment(b.classData.startTime)) ? -1 : 1 })
+
+        /// create class diary embed
         let diaryEmbed = new MessageEmbed()
             .setColor('#4f258a')
             .setTitle(`Diário de Aulas <t:${moment().unix()}:D>:`)
@@ -213,11 +226,13 @@ class Tasks {
             .setTimestamp()
             .setFooter(interactionAuthor.displayName, interactionAuthor.displayAvatarURL());
 
+        /// for each class today
         for (let uniClass of todaysClassesData) {
-        // debugger;
-        let materia: MateriaData = Materias[uniClass.classData.materiaID]
+            // debugger;
+            let materia: MateriaData = Materias[uniClass.classData.materiaID]
             console.debug(uniClass.classData.startTime)
 
+            /// if has class data and materia
             if (uniClass.classData && materia) {
                 let startTime = moment(uniClass.classData.startTime)
 
@@ -225,13 +240,17 @@ class Tasks {
                 let fieldDesc = `\
                     Aula ás <t:${startTime.unix()}:t> horas\n\
                 `
+                /// add class field to embed
                 diaryEmbed.addField(fieldName, fieldDesc, false)
             }
         }
 
-        sendToTextChannel(this.client, process.env.DIARY_CHANNEL_ID, {content: `\u200b`, embeds: [diaryEmbed]})
+        /// send embed to text channel
+        sendToTextChannel(this.client, process.env.DIARY_CHANNEL_ID, { content: `\u200b`, embeds: [diaryEmbed] })
 
+        /// set diary as sent
         todaysDiary.diaryData.dailyReminderSent = true;
+        /// persist changes
         this.client.persistence.upsertDiary(todaysDiary.dateID, todaysDiary.diaryData)
     }
 
