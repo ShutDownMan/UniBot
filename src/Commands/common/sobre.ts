290238cb8cdcb2789810d0601ccdd9e92cb1b63d
ShutDownMan <Jedson_Gabriel@hotmail.com>
2021-11-26 23:09:43 -0300

    IMPROVED sobre command output, showing total lines of code

--- src/Commands/common/sobre.ts
+++ src/Commands/common/sobre.ts
@@ -3,6 +3,7 @@ import { sendEmbed, Color, deleteAfter } from '../../Utils'
 import Logger from '../../Logger'
 import Configs from '../../config.json'
 const log = Logger(Configs.CommandsLogLevel, 'ping.ts')
+import sloc from 'node-sloc'
 
 export const command: Command = {
     name: 'sobre',
@@ -10,6 +11,14 @@ export const command: Command = {
     description: 'Shows info about the bot.',
     allowedRoles: [process.env.EVERYONE_ROLE_ID.toString()],
     run: async (client, message) => {
+        let slocOptions = {
+            path: (process.env.IS_DEV_VERSION === 'true') ? './' : './../src/',
+            extensions: ['ts', 'json'],
+            ignorePaths: ['node_modules'],
+            ignoreDefault: true,
+        }
+        let slocInfo = await sloc(slocOptions);
+
         await sendEmbed(message.channel, {
             color: Color.warn,
             author: {
@@ -17,9 +26,10 @@ export const command: Command = {
             },
             description:
                 '```ini\n' +
-                "Sou um bot criado e mantido por Jedi. Qualquer bug ou problema pode falar com ele!\nPode usar o comando de ajuda " +
-                `[${global.dataState.prefix}ajuda]` +
-                ' para ver os outros comandos possíveis.\n\nObs: O template base para o bot foi feito pelo [github.com/costaluu]\n\nO código é aberto pelo repositório [github.com/ShutDownMan/UniBot]' +
+                'Sou um bot criado e mantido por Jedi. Qualquer bug ou problema pode falar com ele!\nPode usar o comando de ajuda ' +
+                `[${global.dataState.prefix}ajuda]  para ver os outros comandos possíveis.` + 
+                `\n\nTenho ${slocInfo.sloc} linhas de código e ${slocInfo.comments} linhas de comentários, tudo isso em ${slocInfo.files} arquivos.` +
+                '\n\nObs: O template base para o bot foi feito pelo [github.com/costaluu]\n\nO código é aberto pelo repositório [github.com/ShutDownMan/UniBot]' +
                 '\n```',
             fields: [
                 {
